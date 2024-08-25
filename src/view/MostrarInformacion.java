b7ac918c544256a98d277bd81255a7696d78d2ae
Juan David Lopez H <juand.lopezh@uqvirtual.edu.co>
2024-08-24 23:38:26 -0500

    se intenta organizar los ingresos de datos

--- src/view/MostrarInformacion.java
+++ src/view/MostrarInformacion.java
@@ -1,17 +1,29 @@
 import javax.swing.JOptionPane;
 public class MostrarInformacion {
-    public static void main(String[] args) {
-
-    }
     public void mostrarInfo{
         int opcion=0;
-        while(opcion!=5){
+        while(opcion!=6){
             opcion = Integer.parseInt(JOptionPane.showInputDialog(" ingrese 1 si desea crear un plan personalizado" +
                     " ingrese 2 si desea crear un plan de telefonia" +
                     " ingrese 3 si desea crear un plan de television " +
                     " ingrese 4 si desea crear un plan de internet " +
-                    " ingrese 5 si desea salir de la APP"));
-        }
+                    " ingrese 5 si desea modificar un cliente " +
+                    " ingrese 6 si desea salir de la APP"));
+            if (opcion==1){
 
+            }
+            if (opcion==2){
+
+            }
+            if (opcion==3){
+
+            }
+            if (opcion==4){
+
+            }
+            if (opcion==5){
+
+            }
+        }
     }
 }
